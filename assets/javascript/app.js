3c343a143b6f4eb076e0b89234ee6768c6627bdf
Alex Oh <alexhboh@gmail.com>
2019-06-09 11:34:58 -0400

    images pop up now on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,40 @@
-$(document).ready(function(){
-    var topics = ["augmented reality", "computer", "programming", "iphone", "macbook", "apple watch", "programmer", "virtual reality", "facebook", "amazon", "netflix", "google", "kindle"];
+var topics = ["augmented reality", "computer", "programming", "iphone", "macbook", "apple watch", "programmer", "virtual reality", "facebook", "amazon", "netflix", "google", "kindle"];
 
-    console.log(topics);
-    for (var i = 0; i < topics.length; i++){
+function renderButtons(){
+    for (var i = 0; i < topics.length; i++) {
         var topicBtn = $("<button>");
         topicBtn.addClass("topic");
         topicBtn.attr("data-topic", topics[i]);
         topicBtn.text(topics[i]);
-        $("#buttons").append(topicBtn);
-
-        console.log(topicBtn);
+        $("#buttons").append(topicBtn);    
     }
+}
 
-})
+
+function displayGif(){
+    $("#gifs").empty();
+    var gif = $(this).attr("data-topic");
+    console.log(gif);
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ gif +"&api_key=gnnlfsOnrD9U9TqUGuqNcMPbG4DEFJ4z&limit=10 ";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var gifDiv = $("<div class= 'gif'>");
+
+        console.log(response);
+        
+        var giffs = response.data;
+        for (i in giffs){
+            //ratingDiv.append("<p" + giffs[i].rating + "</p>");
+            gifDiv.append("<img src ='"+ giffs[i].images.original.url+"' >");
+        }
+        $("#gifs").append(gifDiv);
+    });
+}
+
+$(document).on("click", ".topic", displayGif);
+renderButtons();
+
+
