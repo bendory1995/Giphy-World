ba0867bc5c9b25b3647d7cc3c213e5ca91076578
Alex Oh <alexhboh@gmail.com>
2019-06-10 10:09:16 -0400

    added an add button and fixed to static image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,31 @@
 var topics = ["augmented reality", "computer", "programming", "iphone", "macbook", "apple watch", "programmer", "virtual reality", "facebook", "amazon", "netflix", "google", "kindle"];
 
+
+
+function displayGif(){
+    $("#gifs").empty();
+
+    var gif = $(this).attr("data-topic");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ gif +"&api_key=gnnlfsOnrD9U9TqUGuqNcMPbG4DEFJ4z&limit=10 ";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        var gifDiv = $("<div class= 'gif'>");
+        
+        var giffs = response.data;
+        for (i in giffs){
+            //gifDiv.append("<span>" + giffs[i].rating + "</span>");
+            gifDiv.append("<img id =  '" + i + "' + src ='"+ giffs[i].images.original_still.url+"' >");
+        }
+        $("#gifs").append(gifDiv);
+    });
+}
+
 function renderButtons(){
+    $("#buttons").empty();
+
     for (var i = 0; i < topics.length; i++) {
         var topicBtn = $("<button>");
         topicBtn.addClass("topic");
@@ -10,31 +35,17 @@ function renderButtons(){
     }
 }
 
-
-function displayGif(){
-    $("#gifs").empty();
-    var gif = $(this).attr("data-topic");
+$("#add-gif").on("click", function(event){
+    console.log("YES");
+    event.preventDefault();
+    var gif = $("#gif-input").val().trim();
     console.log(gif);
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ gif +"&api_key=gnnlfsOnrD9U9TqUGuqNcMPbG4DEFJ4z&limit=10 ";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        var gifDiv = $("<div class= 'gif'>");
-
-        console.log(response);
-        
-        var giffs = response.data;
-        for (i in giffs){
-            //ratingDiv.append("<p" + giffs[i].rating + "</p>");
-            gifDiv.append("<img src ='"+ giffs[i].images.original.url+"' >");
-        }
-        $("#gifs").append(gifDiv);
-    });
-}
+    topics.push(gif);
+    renderButtons();
+    console.log(topics);
+    
+})
 
 $(document).on("click", ".topic", displayGif);
 renderButtons();
-
 
